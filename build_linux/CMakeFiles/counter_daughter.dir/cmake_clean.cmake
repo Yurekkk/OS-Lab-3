1956132971b1b2f0c77ef772c883357fe549d37c
yurekkk <yurrrashka@gmail.com>
2026-01-14 07:09:45 +1000

    Готово

---
+++ build_linux/CMakeFiles/counter_daughter.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/counter_daughter.dir/counter_daughter.c.o"
+  "counter_daughter"
+  "counter_daughter.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/counter_daughter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
